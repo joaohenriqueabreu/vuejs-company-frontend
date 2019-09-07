cd337b66f9e3e3020264349b08a4092baa4ec5a7
João Henrique Abreu <jabreu@sparehire.com>
2019-09-07 16:16:11 -0400

    Pages, routes and components setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import 'Styles/main.scss';
-import Vue from 'vue';
-import HelloWorld from 'Components/HelloWorld.vue';
+/** Base libraries */
+import Vue        from 'vue';
+import Vuex       from 'vuex';
+import VueRouter  from 'vue-router';
+
+/** Entry point */
+import App       from './App.vue';
+
+/** Views */
+import Home       from 'Pages/Home.vue';
+import Company    from 'Pages/Company.vue';
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+/** Components */
+Vue.component('Navbar',   require ('Components/Navbar.vue').default);
+Vue.component('Footer',   require ('Components/Footer.vue').default);
+
+const routes = [
+  { path: '/',              component: Home},
+  { path: '/company/:id',   component: Company }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history' /* Nice URL mode to not display "#" after domain */
+})
 
 new Vue({
-  render: createElement => createElement(HelloWorld),
-  el: '#app'
+  render: createElement => createElement(App),  
+  router,
+  el: '#app',  
 });
